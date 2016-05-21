56e39b166a6b19869d2b89bfb34a2d9309ffdbcc
Edwin Vargas <onix.edwin18@gmail.com>
2016-05-21 19:33:46 +0000

    A little changes

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,3 +1,5 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import css from './styles/style.styl'
 
-import react from 'react';
+render(<p>hi</p>, document.getElementById('root'));
