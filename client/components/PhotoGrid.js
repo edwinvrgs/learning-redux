df855bebaf9a051ba4597c18142baeabf2e5a283
Edwin Vargas <onix.edwin18@gmail.com>
2016-05-22 03:59:52 +0000

    Stuff

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+const PhotoGrid = React.createClass({
+    render() {
+        return (
+            <div className="photo-grid">
+            Hey, soy el PhotoGrid
+            </div>
+        );
+    }
+});
+
+export default PhotoGrid;
