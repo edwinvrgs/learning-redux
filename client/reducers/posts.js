d8df0680a7f8e8e533ffad28e60f17aef3bc0760
Edwin Vargas <onix.edwin18@gmail.com>
2016-06-02 14:34:45 -0430

    Modified posts.js

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,7 +1,16 @@
 const posts = (state = [], action) => {
-    console.log("The post will change");
-    console.log(state, action);
-    return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      console.log("Incrementando likes");
+      const i = action.index;
+      return [
+          ...state.slice(0, i),
+          {...state[i], likes: state[i].likes + 1},
+          ...state.slice(i + 1)
+        ]
+    default:
+      return state;
+  }
 };
 
-export default posts; 
+export default posts;
