ed4b5d75a7158ebf8d06ae5897799e098a4ade0d
Edwin Vargas <onix.edwin18@gmail.com>
2016-06-02 14:33:49 -0430

    Created the photo-comments page

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,13 +1,25 @@
 import React from 'react';
- 
+import Photo from './Photo';
+import Comments from './Comments';
+
 const Single = React.createClass({
-    render() {
-        return (
-            <div className="single-photo">
-            Hey soy una sola foto
-            </div>
-        );
-    }
+  render() {
+    const { postID } = this.props.params;
+    //Obtener el indice del post
+    const i = this.props.posts.findIndex(
+      (post) => post.code === postID
+    );
+    //Obtener el post en cuestion
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postID] || [];
+
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props} />
+      </div>
+    );
+  }
 });
 
 export default Single;
