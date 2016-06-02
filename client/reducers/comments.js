d21d09d336e1cc5c73d7bfbeeff51913a476ca26
Edwin Vargas <onix.edwin18@gmail.com>
2016-06-02 14:34:22 -0430

    Aplied the reducers composition to the comments reducer

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,6 +1,30 @@
+const postComments = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      return [
+        ...state.slice(0, action.index),
+
+        ...state.slice(action.index + 1)
+      ];
+    default:
+      return state;
+  }
+}
+
 const comments = (state = [], action) => {
-    console.log(state, action);
-    return state;
+  if(typeof action.postID !== 'undefined') {
+    return {
+      ...state,
+      [action.postID]: postComments(state[action.postID], action)
+    }
+  }
+
+  return state;
 };
 
 export default comments;
